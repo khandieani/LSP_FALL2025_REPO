4f1ee34a152db0f571614e5b6f151475ab7d90a2
khandieani <khandieani@gmail.com>
2025-10-20 19:37:01 -0400

    Reorganize: Create midterm folder with question2 subfolder

--- LSP_Assignment1/src/org/howard/edu/lsp/midterm/question2/AreaCalculatorTest.java
+++
@@ -1,7 +0,0 @@
-// JUnit test source removed from main src compilation unit because JUnit is not
-// available on the classpath in this workspace by default. If you want to run
-// these as proper JUnit 5 tests, move this file into a test directory and run
-// with JUnit on the classpath (e.g., using Maven/Gradle or the JUnit console
-// launcher). A simple dependency-free test runner exists in
-// AreaCalculatorSimpleTest.java which can be run directly.
-
